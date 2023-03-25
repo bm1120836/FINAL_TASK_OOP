a31569cf9195b4903d729a98b6e69f1f45b6af5e
BM <bm1120836@yandex.ru>
2023-03-25 11:57:28 +0300

    LoggerTerminal

---
+++ TASK7/logger/LoggerTerminal.java
@@ -0,0 +1,12 @@
+package TASK7.logger;
+
+import java.time.LocalDateTime;
+
+public class LoggerTerminal implements LoggerAble {
+
+    @Override
+    public void logg(String msg) {
+        System.out.println(LocalDateTime.now() + msg);
+    }
+
+}
