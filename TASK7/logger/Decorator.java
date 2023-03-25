27b549ed50f3a181815dff0389f8e45edf948857
BM <bm1120836@yandex.ru>
2023-03-25 11:56:33 +0300

    Decorator

---
+++ TASK7/logger/Decorator.java
@@ -0,0 +1,45 @@
+
+package TASK7.logger;
+
+import java.util.List;
+
+import TASK7.model.Fields;
+import TASK7.model.Repository;
+import TASK7.model.User;
+
+public class Decorator implements Repository{
+    private Repository repo;
+    private LoggerTerminal logger;
+    
+    public Decorator(Repository repo, LoggerTerminal logger) {
+        this.repo = repo;
+        this.logger = logger;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<User> res = repo.getAllUsers();
+        return res;
+    }
+
+    @Override
+    public String CreateUser(User user) {
+        String res = repo.CreateUser(user);
+        logger.logg("Date and time of create ");
+        return res;
+    }
+
+    @Override
+    public void UpdateUser(User user, Fields field, String param) {
+        repo.UpdateUser(user, field, param);
+    }
+
+    @Override
+    public void delUser(User user) {
+        repo.delUser(user);
+        logger.logg("Date & time of delete: ");
+    }
+    
+
+    
+}
